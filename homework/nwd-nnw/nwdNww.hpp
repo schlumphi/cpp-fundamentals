357327419e5d364f4d2782403e336615978589a7
schlumphi <j.zarzycki777@gmail.com>
2026-02-08 13:16:37 +0100

    feat: nww task done

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -13,6 +13,9 @@ int NWD(int lhs, int rhs) {
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 || rhs == 0) {
+        return 0;
+    }
+
+    return abs(lhs) * abs(rhs) / NWD(lhs, rhs);
 }
