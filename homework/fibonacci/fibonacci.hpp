9a8bda2c7ad35ab94dc9da7673cde3758405aac1
schlumphi <j.zarzycki777@gmail.com>
2026-02-08 12:21:57 +0100

    feat: fibonacci iterative

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    } else if (sequence == 1) {
+        return 1;
+    }
+    
+    auto fn2{0};
+    auto fn1{1};
+    for (auto i = 2; i < sequence; ++i) {
+        auto tmp = fn1 + fn2;
+        fn2 = fn1;
+        fn1 = tmp;
+    }
+
+    return fn1 + fn2;
 }
 
 int fibonacci_recursive(int sequence) {
