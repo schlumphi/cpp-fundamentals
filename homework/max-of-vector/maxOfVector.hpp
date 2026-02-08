07a9a682ae12f6507ec736f53ab902538a0ecce0
schlumphi <j.zarzycki777@gmail.com>
2026-02-08 15:46:34 +0100

    fix: edge case when input vector is empty

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,10 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
+    if (vec.empty()) {
+        return std::numeric_limits<int>::min();
+    }
+
     auto max(vec.front());
     for (const auto elem : vec) {
         if (elem >= max) {
