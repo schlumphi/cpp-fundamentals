c176d3e9a12f3196f9ecc7ea2ca909f078986dbd
schlumphi <j.zarzycki777@gmail.com>
2026-02-08 13:11:59 +0100

    feat: nwd computation

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    auto a = abs(lhs);
+    auto b = abs(rhs);
+    while (b != 0) {
+        auto c{a % b};
+        a = b;
+        b = c;
+    }
+
+    return a;
 }
 
 int NWW(int lhs, int rhs) {
