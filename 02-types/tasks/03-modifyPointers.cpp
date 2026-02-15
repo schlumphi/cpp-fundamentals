aa3e566e388b02153d9de43ac58072e288d776dd
Jakub Zarzycki <j.zarzycki777@gmail.com>
2026-02-15 10:19:57 +0100

    feat: modifyPointers task done

--- 02-types/tasks/03-modifyPointers.cpp
+++ 02-types/tasks/03-modifyPointers.cpp
@@ -4,10 +4,17 @@
 // foo() should modify value under passed pointer to 10
 // bar() should modify value under passed pointer to 20
 // Can we have a pointer to const or a const pointer?
+auto foo(int* const data) -> void {
+    *data = 10;
+}
+
+auto bar(int* const data) -> void {
+    *data = 20;
+}
 
 int main() {
     int number = 5;
-    int* pointer = &number;
+    int* const pointer = &number;
     std::cout << number << '\n';
     foo(&number);
     std::cout << number << '\n';
